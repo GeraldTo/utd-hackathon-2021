e08c4865640f96d31550964fd4c12f189b5f0470
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-11-14 11:07:48 -0600

    logs are tracked

--- client/src/components/Body/LogTab/Logs.tsx
+++ client/src/components/Body/LogTab/Logs.tsx
@@ -7,39 +7,45 @@ export interface LogTabProps {
   response: null | ClientResponse
 }
 
-
 export default function Logs(props: LogTabProps) {
-    const logArr = []
-    function addLog(x: null | ClientResponse){
-        
-        return x;
+  const [logArr, setLogArr] = React.useState<ClientResponse[]>([])
+  const [headerNames, setHeaderNames] = React.useState<string[]>([])
+  const maxLen = 15
+  React.useEffect(() => {
+    if (props.response) {
+      let newArr = [props.response].concat([...logArr])
+      if (newArr.length > maxLen) {
+        newArr.pop()
       }
-    
+      setLogArr(newArr)
+      let newHeaders = props.response.map((e) => e.operationId)
+      if (newHeaders.length > headerNames.length) setHeaderNames(newHeaders)
+    }
+  }, [props.response])
+
   if (props.response) {
     const el = props.response! as object
     return (
-      <div className={styles.Box}>
+      <div>
         <h1>Responses</h1>
         <Table striped bordered hover>
           <thead>
             <tr>
-              { props.response.map(function (e, i) {
-                  const result = e.operationId.replace(/_/g, ' ')
-                  const finalResult =
-                    result.charAt(0).toUpperCase() + result.slice(1)
-                  return <th key={i}>{finalResult}</th>
-              })
-              }
+              {headerNames.map(function (e, i) {
+                const result = e.replace(/_/g, ' ')
+                const finalResult =
+                  result.charAt(0).toUpperCase() + result.slice(1)
+                return <th key={i}>{finalResult}</th>
+              })}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              { props.response.map(function (e, i) {
-                const element = e.flowRate
-                return <td key={i}>{Math.round(element * 100) / 100}</td>
+            {logArr.map(function (row, i) {
+              const cols = row.map(function (col, j) {
+                return <td key={j}>{Math.round(col.flowRate * 100) / 100}</td>
               })
-              }
-            </tr>
+              return <tr key={i}>{cols}</tr>
+            })}
           </tbody>
         </Table>
       </div>
